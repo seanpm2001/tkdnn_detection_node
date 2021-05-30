b66496de81c091f45cdc9a127a5c893121e9e2f0
Rodja Trappe <rodja@zauberzeug.com>
2021-05-30 05:21:06 +0200

    some cleanup and helpers

--- detection_node.dockerfile
+++ detection_node.dockerfile
@@ -7,27 +7,16 @@ RUN curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-
     ln -s /opt/poetry/bin/poetry && \
     poetry config virtualenvs.create false
 
-# needed for opencv 
-RUN apt-get update && apt-get -y install libgl1-mesa-dev
-
-WORKDIR /learning_loop_node/
-
-COPY ./learning_loop_node/ ./
-
 WORKDIR /app/
 
-RUN ln -s /learning_loop_node /app/learning_loop_node && ls -lha learning_loop_node/
-
-COPY ./detection_node/pyproject.toml ./detection_node/poetry.lock* ./
-
-RUN poetry add opencv-python
-RUN poetry update
+COPY ./pyproject.toml ./poetry.lock* ./
 
 # Allow installing dev dependencies to run tests
 ARG INSTALL_DEV=false
 RUN bash -c "if [ $INSTALL_DEV == 'true' ] ; then poetry install --no-root ; else poetry install --no-root --no-dev ; fi"
 
-COPY ./detection_node/ /app
+COPY ./detection_node/ ./
 ENV PYTHONPATH=/app
 
 EXPOSE 80
+CMD bash
---
+++ docker.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]
+then
+    echo "Usage:"
+    echo
+    echo "  `basename $0` (b | build)        Build"
+    echo "  `basename $0` (r | run)          Run"
+    echo "  `basename $0` (s | stop)         Stop"
+    echo "  `basename $0` (k | kill)         Kill"
+    echo "  `basename $0` rm                 Remove"
+    echo
+    echo "  `basename $0` (l | log)                 Show log tail (last 100 lines)"
+    echo "  `basename $0` (e | exec)     <command>  Execute command"
+    echo "  `basename $0` (a | attach)              Attach to container with shell"
+    echo
+    echo "Arguments:"
+    echo
+    echo "  command       Command to be executed inside a container"
+    exit
+fi
+
+cmd=$1
+cmd_args=${@:2}
+case $cmd in
+    b | build)
+        docker build -f detection_node.dockerfile . -t tkdnn_detection_node $cmd_args
+        ;;
+    r | run)
+        docker run -it --rm  --name tkdnn_detection_node tkdnn_detection_node $cmd_args
+        ;;
+    s | stop)
+        docker stop tkdnn_detection_node $cmd_args
+        ;;
+    k | kill)
+        docker kill tkdnn_detection_node $cmd_args
+        ;;
+    l | log | logs)
+        docker logs -f --tail 100 $cmd_args tkdnn_detection_node
+        ;;
+    e | exec)
+        docker exec $cmd_args tkdnn_detection_node
+        ;;
+    a | attach)
+        docker exec $cmd_args tkdnn_detection_node /bin/bash
+        ;;
+    *)
+        echo "Unsupported command \"$cmd\""
+        exit 1
+esac
+

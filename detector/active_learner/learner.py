91850b7e853396396cec220d90a8d199e5ed7cc5
Rodja Trappe <rodja@zauberzeug.com>
2021-10-24 12:31:08 +0200

    began implementing active learning for point detections

--- detector/active_learner/learner.py
+++ detector/active_learner/learner.py
@@ -1,19 +1,20 @@
-from typing import List
+from typing import List, Union
 from active_learner.observation import Observation
 from icecream import ic
 from box_detection import BoxDetection
 from detections import Detections
+from point_detection import PointDetection
 
 
 class Learner:
     def __init__(self):
         self.reset_time = 3600
-        self.low_conf_box_observations: List[Observation] = []
+        self.low_conf_observations: List[Observation] = []
         self.iou_threshold = 0.5
 
     def forget_old_detections(self):
-        self.low_conf_box_observations = [detection
-                                    for detection in self.low_conf_box_observations
+        self.low_conf_observations = [detection
+                                    for detection in self.low_conf_observations
                                     if not detection.is_older_than(self.reset_time)]
 
     def add_box_detections(self, box_detections: List[BoxDetection]) -> List[str]:
@@ -22,23 +23,30 @@ class Learner:
     def add_detections(self, detections: Detections) -> List[str]:
         active_learning_causes = set()
 
-        for box in detections.box_detections:
-            if box.confidence < .3 or box.confidence > .6:
+        for detection in detections.box_detections + detections.point_detections:
+            if detection.confidence < .3 or detection.confidence > .6:
                 continue
 
-            similar_detections = self.find_similar_observations(box)
-            if(any(similar_detections)):
-                [sd.update_last_seen() for sd in similar_detections]
+            similar = self.find_similar_observations(detection)
+            if(any(similar)):
+                [s.update_last_seen() for s in similar]
             else:
-                self.low_conf_box_observations.append(Observation(box))
+                self.low_conf_observations.append(Observation(detection))
                 active_learning_causes.add('lowConfidence')
 
         return list(active_learning_causes)
 
-    def find_similar_observations(self, new_detection: BoxDetection):
+    def find_similar_observations(self, new_detection: Union[BoxDetection]):
         return [
             observation
-            for observation in self.low_conf_box_observations
+            for observation in self.low_conf_observations
             if observation.detection.category_name == new_detection.category_name
-            and observation.detection.intersection_over_union(new_detection) >= self.iou_threshold
+            and self.similar(observation.detection, new_detection)
         ]
+
+    def similar(self, a, b) -> bool:
+        if a is BoxDetection and b is BoxDetection:
+            return a.intersection_over_union(b) >= self.iou_threshold
+        if a is PointDetection and b is PointDetection:
+            return a.distance(b) < 10
+        return False
--- detector/tests/active_learner/test_active_learning.py
+++ detector/tests/active_learner/test_active_learning.py
@@ -6,6 +6,7 @@ from active_learner import learner as l
 from icecream import ic
 import time
 from box_detection import BoxDetection
+from point_detection import PointDetection
 
 dirt_detection = BoxDetection('dirt', 0, 0, 100, 100, 'xyz', .3)
 second_dirt_detection = BoxDetection('dirt', 0, 20, 10, 10, 'xyz', .35)
@@ -15,33 +16,33 @@ conf_too_low_detection = BoxDetection('dirt', 0, 0, 100, 100, 'xyz', .29)
 
 def test_learner_confidence():
     learner = l.Learner()
-    assert len(learner.low_conf_box_observations) == 0
+    assert len(learner.low_conf_observations) == 0
 
     active_learning_cause = learner.add_box_detections([dirt_detection])
     assert active_learning_cause == ['lowConfidence'], \
         f'Active Learning should be done due to lowConfidence'
-    assert len(learner.low_conf_box_observations) == 1, 'Detection should be stored'
+    assert len(learner.low_conf_observations) == 1, 'Detection should be stored'
 
     active_learning_cause = learner.add_box_detections([dirt_detection])
-    assert len(learner.low_conf_box_observations) == 1, f'Detection should already be stored'
+    assert len(learner.low_conf_observations) == 1, f'Detection should already be stored'
     assert active_learning_cause == []
 
     active_learning_cause = learner.add_box_detections([conf_too_low_detection])
-    assert len(learner.low_conf_box_observations) == 1, f'Confidence of detection too low'
+    assert len(learner.low_conf_observations) == 1, f'Confidence of detection too low'
     assert active_learning_cause == []
 
     active_learning_cause = learner.add_box_detections([conf_too_high_detection])
-    assert len(learner.low_conf_box_observations) == 1, f'Confidence of detection too high'
+    assert len(learner.low_conf_observations) == 1, f'Confidence of detection too high'
     assert active_learning_cause == []
 
 
 def test_add_second_detection_to_learner():
     learner = l.Learner()
-    assert len(learner.low_conf_box_observations) == 0
+    assert len(learner.low_conf_observations) == 0
     learner.add_box_detections([dirt_detection])
-    assert len(learner.low_conf_box_observations) == 1, 'Detection should be stored'
+    assert len(learner.low_conf_observations) == 1, 'Detection should be stored'
     learner.add_box_detections([second_dirt_detection])
-    assert len(learner.low_conf_box_observations) == 2, 'Second detection should be stored'
+    assert len(learner.low_conf_observations) == 2, 'Second detection should be stored'
 
 
 def test_update_last_seen():
@@ -53,21 +54,21 @@ def test_update_last_seen():
 
 def test_forget_old_detections():
     learner = l.Learner()
-    assert len(learner.low_conf_box_observations) == 0
+    assert len(learner.low_conf_observations) == 0
 
     active_learning_cause = learner.add_box_detections([dirt_detection])
     assert active_learning_cause == [
         'lowConfidence'], f'Active Learning should be done due to lowConfidence.'
 
-    assert len(learner.low_conf_box_observations) == 1
+    assert len(learner.low_conf_observations) == 1
 
-    learner.low_conf_box_observations[0].last_seen = datetime.now() - timedelta(minutes=30)
+    learner.low_conf_observations[0].last_seen = datetime.now() - timedelta(minutes=30)
     learner.forget_old_detections()
-    assert len(learner.low_conf_box_observations) == 1
+    assert len(learner.low_conf_observations) == 1
 
-    learner.low_conf_box_observations[0].last_seen = datetime.now() - timedelta(hours=1, minutes=1)
+    learner.low_conf_observations[0].last_seen = datetime.now() - timedelta(hours=1, minutes=1)
     learner.forget_old_detections()
-    assert len(learner.low_conf_box_observations) == 0
+    assert len(learner.low_conf_observations) == 0
 
 
 def test_active_learner_extracts_from_json():
@@ -101,3 +102,15 @@ def test_active_learner_extracts_from_json():
         [BoxDetection.from_dict(_detection) for _detection in detections])
 
     assert active_learning_cause == ['lowConfidence']
+
+
+def test_ignoring_similar_points():
+    learner = l.Learner()
+    active_learning_cause = learner.add_point_detections([PointDetection('point', 100,100,'xyz', 0.3)])
+    assert active_learning_cause == ['lowConfidence'], \
+        f'Active Learning should be done due to low confidence'
+    assert len(learner.low_conf_observations) == 1, 'detection should be stored'
+
+    active_learning_cause = learner.add_point_detections([PointDetection('point', 104,98, 'xyz', 0.3)])
+    assert len(learner.low_conf_observations) == 1, f'detection should already be stored'
+    assert active_learning_cause == []
